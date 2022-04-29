44e2181fc3bf81a888200fbab540be545fcadeb4
26268 <2741970693@qq.com>
2022-04-29 11:04:07 +0800

    1

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,14 +1,17 @@
 <template>
-  <vc-viewer :scene3DOnly="true" @ready="onViewerReady">
-    <flyControl  />
+    <vc-viewer :scene3DOnly="true" @ready="onViewerReady" :imageryProvider="imageryProvider">
+    <vc-terrain-provider-arcgis ></vc-terrain-provider-arcgis>
+      <flyControl  />
     <echartStyle />
   </vc-viewer>
+  
 </template>
 <script setup>
-import {ref,computed} from "vue";
-import { VcViewer } from "vue-cesium";
+import { ref, computed } from "vue";
+import { VcViewer,VcTerrainProviderArcgis } from "vue-cesium";
 import flyControl from "./flyControl.vue";
 import echartStyle from "./echartStyle.vue";
+const imageryProvider = ref(null);
 const onViewerReady = (ready) => {
   const viewer = ready.viewer;
   //移除报错
@@ -17,6 +20,8 @@ const onViewerReady = (ready) => {
   //取消默认双击事件
   viewer.screenSpaceEventHandler.setInputAction(function () {},
   Cesium.ScreenSpaceEventType.LEFT_DOUBLE_CLICK);
-
+  imageryProvider.value = new Cesium.ArcGisMapServerImageryProvider({
+    url: "https://services.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer",
+  });
 };
 </script>
