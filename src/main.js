4bc46237343105466bffcf00088b510b8eff55ee
26268 <2741970693@qq.com>
2022-04-30 19:43:14 +0800

    1

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import VueCesium from 'vue-cesium'
 import 'vue-cesium/dist/index.css'
-import 'cesium/Build/Cesium/Widgets/widgets.css'
+// import 'cesium/Build/Cesium/Widgets/widgets.css'
 const app = createApp(App)
 app.use(VueCesium)
 app.mount('#app')
