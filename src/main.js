b3a47a8478f80a24fd77a4dd165b80f344b40189
26268 <2741970693@qq.com>
2022-05-02 16:00:02 +0800

    1

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import { createApp } from 'vue'
-import App from './App.vue'
+import Main from './components/Main.vue'
 import 'cesium/Build/Cesium/Widgets/widgets.css'
-const app = createApp(App)
+const app = createApp(Main)
 import * as Cesium from 'cesium'
 window.Cesium = Cesium
 window.main={}
