ff959c6671399ab5f353f34ee755e70036db2620
26268 <2741970693@qq.com>
2022-05-02 15:57:49 +0800

    fix

--- src/components/flyControl.vue
+++ src/components/flyControl.vue
@@ -4,7 +4,11 @@
     <button @click="next">下一站</button>
     <button @click="start">开始漫游</button>
     <button @click="stop">停止漫游</button>
-    漫游间隔：<input style="width:40px" type="number" v-model="tourInterval">秒
+    漫游间隔：<input
+      style="width: 40px"
+      type="number"
+      v-model="tourInterval"
+    />秒
   </div>
 </template>
 <script setup>
@@ -13,7 +17,7 @@ const start = ref();
 const stop = ref();
 const last = ref();
 const next = ref();
-const tourInterval=ref(10);
+const tourInterval = ref(10);
 let fly, tours;
 onMounted(() => {
   const viewer = main.viewer;
@@ -39,7 +43,7 @@ onMounted(() => {
           // scaleByDistance: new Cesium.NearFarScalar(1.5e2, 2.0, 1.5e7, 0.5),
           distanceDisplayCondition: new Cesium.DistanceDisplayCondition(
             10.0,
-            39999
+            999999
           ),
         });
       });
@@ -86,24 +90,17 @@ onMounted(() => {
     const img = new Image();
     img.alt = tour.name;
     img.src = img_url;
-    const check = function () {
-      // 只要任何一方大于0
-      // 表示已经服务器已经返回宽高
-      if (img.width > 0 || img.height > 0) {
-        img.style = `width:100%;height:auto;object-fit: cover;`;
 
-        const div = document.createElement("div");
-        div.innerHTML = ` 当前站点：${tour.name}<br>
+    img.style = `width:100%;height:auto;object-fit: cover;`;
+
+    const div = document.createElement("div");
+    div.innerHTML = ` 当前站点：${tour.name}<br>
       介绍：${tour.properties.PopupInfo._value}<br>`;
-        div.appendChild(img);
+    div.appendChild(img);
 
-        tour.description = div.innerHTML;
-        clearInterval(set);
-      }
-    };
-    const set = setInterval(check, 40);
+    tour.description = div.innerHTML;
 
-    camera.flyToBoundingSphere(new Cesium.BoundingSphere(n, 2000), {
+    camera.flyToBoundingSphere(new Cesium.BoundingSphere(n, 4000), {
       duration: 1,
       offset: new Cesium.HeadingPitchRange(0, -0.1, 0),
       complete: () => {
